d663660cdfa7905a7c36ca1c5257d98c16e15a61
shlapique <gordsdima@gmail.com>
2022-12-22 22:49:09 +0300

    init

---
+++ mnk.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+f_noise = function(theta, )
+{
+
+}
+
+x = c(-4 + 8/40, 4, 40)
+
+theta = c(0, 5, 6, 0.06)
+
+e_k <- rnorm(n = 40, mean = 0, sd = 2.5)
+
+#noise func
+for(i in e_k)
+{
+    s[i] <- 
+
+
+
+
+}
